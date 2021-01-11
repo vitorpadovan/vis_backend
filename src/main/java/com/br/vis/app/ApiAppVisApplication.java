41e337252d02b7b0d4230dd988459700669363d5
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-01-11 14:00:48 -0300

    Projeto inicial

---
+++ src/main/java/com/br/vis/app/ApiAppVisApplication.java
@@ -0,0 +1,13 @@
+package com.br.vis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppVisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAppVisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/vis/app/ApiAppVisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppVisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
