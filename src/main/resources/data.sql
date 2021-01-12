abc75402dd3b029e24f91e243a10eb751c49afd0
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-01-12 18:35:08 -0300

    Banco de dados inicial

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO CadaOper(operNomeOper, operLogiOper, operSenhOper, operDataCada)
+VALUES('Vitor Padovan','vitor','$2a$10$Jiasq.OCeMP.1/Iz4AweQesGsqjLlGlBMlCtljkoM4DHmyz9/07NG','2021-01-07 23:10'),
+('Vitor Padovan','vinicius','$2a$10$Jiasq.OCeMP.1/Iz4AweQesGsqjLlGlBMlCtljkoM4DHmyz9/07NG','2021-01-07 23:11');
+INSERT INTO DireOper VALUES(1,'ROLE_TESTE','ROLE_TESTE');
+INSERT INTO JTDireOper VALUES(1,1);
